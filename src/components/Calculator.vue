075b0d3efa961592ee5876eac70537cb93307006
Mitul Patel <mitul.patel@1rivet.local>
2022-12-27 12:25:40 +0530

    feat: scss setup

---
+++ src/components/Calculator.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="tip-calculator">
+    Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount /
+    person Total / person Reset
+
+    <div class="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a
+        href="#">Your Name Here</a>.
+    </div>
+  </section>
+</template>
+
+<script lang="tsx">
+export default {
+  name: "TipCalculator",
+  data() {
+    return {
+      tip: 0,
+    };
+  },
+};
+</script>
+<style lang="scss"></style>
