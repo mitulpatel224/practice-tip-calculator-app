83403d35299dd7195e10caa0445a5efd1d33e461
Mitul Patel <mitul.patel@1rivet.local>
2022-12-27 13:21:26 +0530

    feat: html structure for calculator component

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,32 @@ import TipCalculator from "./components/Calculator.vue";
   <main>
     <TipCalculator />
   </main>
+
+  <footer>
+    <div class="attribution">
+      Challenge by
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+        Frontend Mentor
+      </a>.
+      Coded by
+      <a href="#">
+        Your Name Here
+      </a>.
+    </div>
+  </footer>
 </template>
 
 <style lang="scss">
     @import "style.scss";
-</style>
+
+    #app {
+      height: 100%;
+    }
+
+    main {
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  </style>
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,12 +1,58 @@
 <template>
   <section class="tip-calculator">
-    Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount /
-    person Total / person Reset
+    <!-- BILL AMOUNT -->
+    <div class="bill-wrapper">
+      <h2>Bill</h2>
+      <div class="ctrl bill-ctrl">
+        <input type="number" name="bill" id="bill" />
+      </div>
+    </div>
 
-    <div class="attribution">
-      Challenge by
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. Coded by <a
-        href="#">Your Name Here</a>.
+    <!-- TIP SELECTION -->
+    <div class="tips-wrapper">
+      <h2>Select Tip %</h2>
+      <div class="tips">
+        <button class="btn btn-tip active">5%</button>
+        <button class="btn btn-tip">10%</button>
+        <button class="btn btn-tip">15%</button>
+        <button class="btn btn-tip">25%</button>
+        <button class="btn btn-tip">50%</button>
+        <div class="custom-tip-wrapper">
+          <button v-if="!customTip" class="btn btn-tip">Custom</button>
+          <input v-if="customTip" type="number" name="customTip" id="custom-tip" />
+        </div>
+      </div>
+    </div>
+
+    <!-- PERSON SELECTION -->
+    <div class="persons-wrapper">
+      <h2>Number of People</h2>
+      <div class="ctrl person-ctrl">
+        <input type="number" name="bill" id="bill" />
+      </div>
+    </div>
+
+    <!-- TIP CALCULATION -->
+    <div class="tip-amount-wrapper">
+      <div class="tip-amount">
+        <div class="label">
+          <h2>Tip Amount</h2>
+          <p>/ person</p>
+        </div>
+        <div class="value">{{ tipValue }}</div>
+      </div>
+
+      <div class="total-amount">
+        <div class="label">
+          <h2>Total</h2>
+          <p>/ person Reset</p>
+        </div>
+        <div class="value">{{ totalValue }}</div>
+      </div>
+
+      <div class="tip-reset">
+        <button class="btn btn-reset">RESET</button>
+      </div>
     </div>
   </section>
 </template>
@@ -16,9 +62,10 @@ export default {
   name: "TipCalculator",
   data() {
     return {
-      tip: 0,
+      tipValue: 0,
+      totalValue: 0,
+      customTip: false
     };
   },
 };
 </script>
-<style lang="scss"></style>
